235a4d5f0d373ae13b5029247c95efd48624c228
hhwsun <hhwsun@gmail.com>
2025-05-31 02:24:39 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 if [ $# -eq 0 ]; then
     echo "Usage: $0 <option>"
@@ -12,8 +12,7 @@ case "$OPTION" in
         curl -sfL https://hhwsun.github.io/test/hello.sh | sh -s
         ;;
     install)
-        if command -v k3s &> /dev/null
-        then
+        if command -v k3s > /dev/null 2>&1; then
             echo "k3s was already installed."
         else
             echo "Install k3s: curl -sfL https://hhwsun.github.io/test/install.sh | K3S_TOKEN=MYK3SSECRET sh -s - server --cluster-init"
@@ -21,8 +20,7 @@ case "$OPTION" in
         fi
         ;;
     uninstall)
-        if command -v k3s &> /dev/null
-        then
+        if command -v k3s > /dev/null 2>&1; then
             echo "Uninstall k3s: curl -sfL https://hhwsun.github.io/test/uninstall.sh | sh -s"
             curl -sfL https://hhwsun.github.io/test/uninstall.sh | sh -s
         else
