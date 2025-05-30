3fa14c1c527233c0f23e88bf5f4d7608784fb861
hhwsun <hhwsun@gmail.com>
2025-05-30 23:39:06 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <option>"
+    exit 1
+fi
+
+OPTION=$1
+
+case "$OPTION" in
+    install)
+        echo "curl -sfL https://hhwsun.github.io/test/install.sh | sh"
+        ;;
+    uninstall)
+        echo "curl -sfL https://hhwsun.github.io/test/uninstall.sh | sh"
+        ;;
+    *)
+        echo "Unknown option: $OPTION"
+        echo "Available options: install, uninstall"
+        exit 1
+        ;;
+esac
